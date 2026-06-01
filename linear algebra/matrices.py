850266aff01c38bcb0a75e487ca0f0c02307dcaf
Ajay lodhi <satyamlodhi123@gmail.com>
2026-06-01 21:21:54 +0530

    lambda's solution

--- linear algebra/matrices.py
+++ linear algebra/matrices.py
@@ -1,2 +1,26 @@
-class Checking:
-    def __init__(self):
+# class Matrix:
+#     def __init__(self, row, column, type):
+#         if isinstance(row, list):
+#             self.row = row
+#         if isinstance(column, list):
+#             self.column = column
+#         self.two_d = type
+#
+#
+#     def
+def show(mat, n, m):
+  for idx in range(n):
+    for jdx in range(m):
+      print(mat[idx*m+jdx], end=" ")
+    print()
+  print()
+
+xs = range(1, 6+1)
+show(xs, 2, 3)
+# 1 2 3
+# 4 5 6
+
+# show(xs, 3, 2)
+# # 1 2
+# # 3 4
+# # 5 6
