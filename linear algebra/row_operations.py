e5ac428021223630316c2ace7b8564aa434f162e
Ajay lodhi <satyamlodhi123@gmail.com>
2026-06-03 19:02:16 +0530

    multiply and addition

--- linear algebra/row_operations.py
+++ linear algebra/row_operations.py
@@ -1,7 +1,7 @@
 import pprint
 
 
-# multiply or divide a row by constant
+
 # how to add or subtract row
 
 # how to switch row in the matrix
@@ -18,12 +18,24 @@ class RowOperations:
         self.rows[row1], self.rows[row2] = self.rows[row2], self.rows[row1]
         return self.rows
 
+    # multiply or divide a row by constant
+    def scaler_multiply(self, number):
+        self.rows = [[i * number for i in a] for a in self.rows]
+        return self.rows
+
+    def addition(self, number):
+        self.rows = [[number + a for a in i]for i in self.rows]
+        return self.rows
+
+
 
 mat = RowOperations([[1,2,3], [4,5,6]])
 print(mat)
 mat.switching_rows(0,1)
 print(mat)
-
-
+mat.scaler_multiply(2)
+print(mat)
+mat.addition(3)
+print(mat)
 
 
